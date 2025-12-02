69b47ec26588bab6f27c9042c3067d659963214c
Keegan Rhodes <keegan.rhodes1@gehealthcare.com>
2025-12-02 15:20:25 -0600

    part_one_complete

---
+++ day_1/secret_entrance.py
@@ -0,0 +1,24 @@
+MAX_NUMBER = 99
+
+def main():
+    current_number = 50
+    lines = open('secret_entrance.txt').read().splitlines()
+    number_of_zeros = 0
+    for line in lines:
+        number = int(line[1:])
+        if line[0] == 'L':
+            current_number -= number % (MAX_NUMBER + 1)
+            if current_number < 0:
+                current_number += (MAX_NUMBER + 1)
+        elif line[0] == 'R':
+            current_number += number % (MAX_NUMBER + 1)
+            if current_number > MAX_NUMBER:
+                current_number -= (MAX_NUMBER + 1)
+        if current_number == 0:
+            number_of_zeros += 1
+    print(f'answer: {number_of_zeros}')
+
+
+
+if __name__ == "__main__":
+    main()
